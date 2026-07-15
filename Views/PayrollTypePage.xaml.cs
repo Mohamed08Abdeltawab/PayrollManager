d8104b01441d9a04f6570dd552983520775e97f3
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-07-15 12:23:36 +0300

    add payroll type page ui

--- Views/PayrollTypePage.xaml.cs
+++ Views/PayrollTypePage.xaml.cs
@@ -1,9 +1,31 @@
-namespace PayrollManager.Views;
+using System;
+using Microsoft.Maui.Controls;
 
-public partial class PayrollTypePage : ContentPage
+namespace PayrollManager.Views
 {
-	public PayrollTypePage()
-	{
-		InitializeComponent();
-	}
+    public partial class PayrollTypePage : ContentPage
+    {
+        public PayrollTypePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnAddPayrollTypeTapped(object sender, EventArgs e)
+        {
+            // Navigate to the payroll type editor in insert mode (id = 0)
+            await Shell.Current.GoToAsync(nameof(PayrollTypeEditorPage));
+        }
+
+        private async void OnEditTapped(object sender, EventArgs e)
+        {
+            // Navigate to the payroll type editor in update mode
+            await Shell.Current.GoToAsync(nameof(PayrollTypeEditorPage));
+        }
+
+        private async void OnExportTapped(object sender, EventArgs e)
+        {
+            // Statically handle Excel/CSV export click event for now
+            await DisplayAlert("تصدير البيانات", "جاري تحضير ملف Excel/CSV...", "موافق");
+        }
+    }
 }
